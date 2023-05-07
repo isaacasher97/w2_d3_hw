7600126d164deedb6bd4518fc1fa559f003fd05f
isaacasher97 <isaac.asher97@gmail.com>
2023-05-07 14:18:13 -0400

    Disemvoweling trolls answered

--- solutions.js
+++ solutions.js
@@ -22,18 +22,43 @@
 
 //2. Write a function named oddNumberCount which receives one argument that should be a number. The function should return how many positive odd numbers there are below the number passed into the argument.
 
-function oddNumberCount(number) {
-    let count = 0;
-    for (let i = 1; i < number; i+=1) {
-      if (i % 2 !== 0) {
-        count+=1;
+// function oddNumberCount(number) {
+//     let count = 0;
+//     for (let i = 1; i < number; i+=1) {
+//       if (i % 2 !== 0) {
+//         count+=1;
+//       }
+//     }
+//     return count;
+//   }
+
+// //   console.log(oddNumberCount(7))
+// //   console.log(oddNumberCount(16))
+
+// oddNumberCount(7) // => returns 3 (because [1, 3, 5] are the positive odd numbers below 7)
+// oddNumberCount(16) // => returns 8 (because [1, 3, 5, 7, 9, 11, 13, 15] are the positive odd numbers below 7)
+
+
+// 3. Write a function named trollsBeGone which receives one argument that should be a sentence (a string). The function should return a new string with all the vowels removed from the original string.
+
+function trollsBeGone(sentence) {
+    // Define a string of vowels that we want to remove
+    const vowels = "aeiouAEIOU";
+    // Initialize a new string to hold the result without vowels
+    let newSentence = "";
+    // Loop through each character in the original sentence
+    for (let i = 0; i < sentence.length; i++) {
+      const char = sentence[i];  // Get the character at the current index
+      // If the character is not a vowel, add it to the new sentence
+      if (!vowels.includes(char)) {
+        newSentence += char;
       }
     }
-    return count;
+    // Return the new sentence without vowels
+    return newSentence;
   }
+  
 
-//   console.log(oddNumberCount(7))
-//   console.log(oddNumberCount(16))
+// console.log(trollsBeGone("This website is for losers LOL")) 
 
-oddNumberCount(7) // => returns 3 (because [1, 3, 5] are the positive odd numbers below 7)
-oddNumberCount(16) // => returns 8 (because [1, 3, 5, 7, 9, 11, 13, 15] are the positive odd numbers below 7)
+trollsBeGone("This website is for losers LOL") // => returns "Ths wbst s fr lsrs LL"
