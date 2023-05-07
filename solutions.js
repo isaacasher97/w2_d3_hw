5651e07adab815d08f5ab7df57a9afe63f39936e
isaacasher97 <isaac.asher97@gmail.com>
2023-05-07 14:50:07 -0400

    Bank account summary answered

--- solutions.js
+++ solutions.js
@@ -41,24 +41,62 @@
 
 // 3. Write a function named trollsBeGone which receives one argument that should be a sentence (a string). The function should return a new string with all the vowels removed from the original string.
 
-function trollsBeGone(sentence) {
-    // Define a string of vowels that we want to remove
-    const vowels = "aeiouAEIOU";
-    // Initialize a new string to hold the result without vowels
-    let newSentence = "";
-    // Loop through each character in the original sentence
-    for (let i = 0; i < sentence.length; i++) {
-      const char = sentence[i];  // Get the character at the current index
-      // If the character is not a vowel, add it to the new sentence
-      if (!vowels.includes(char)) {
-        newSentence += char;
-      }
-    }
-    // Return the new sentence without vowels
-    return newSentence;
-  }
+// function trollsBeGone(sentence) {
+//     // Define a string of vowels that we want to remove
+//     const vowels = "aeiouAEIOU";
+//     // Initialize a new string to hold the result without vowels
+//     let newSentence = "";
+//     // Loop through each character in the original sentence
+//     for (let i = 0; i < sentence.length; i++) {
+//       const char = sentence[i];  // Get the character at the current index
+//       // If the character is not a vowel, add it to the new sentence
+//       if (!vowels.includes(char)) {
+//         newSentence += char;
+//       }
+//     }
+//     // Return the new sentence without vowels
+//     return newSentence;
+//   }
   
 
-// console.log(trollsBeGone("This website is for losers LOL")) 
+// // console.log(trollsBeGone("This website is for losers LOL")) 
 
-trollsBeGone("This website is for losers LOL") // => returns "Ths wbst s fr lsrs LL"
+// trollsBeGone("This website is for losers LOL") // => returns "Ths wbst s fr lsrs LL"
+
+// 4. Make an object named bankInfo with your banking info that includes the keys listed below. Each key should have a number value that can either be negative or positive (e.g. -500 or 500)
+
+const bankInfo = {
+    savings: 600,
+    checking: 800,
+    moneyMarket: 200,
+    creditCard: -2000,
+  };
+  
+  function bankAccountSummary(bankInfo) {
+    let total = 0;
+    for (let account in bankInfo) {
+      total += bankInfo[account];
+    }
+    return total;
+  }
+  
+  const totalBalance = bankAccountSummary(bankInfo);
+
+  //   console.log(totalBalance); // Output: -400
+  bankAccountSummary(bankInfo) // returns -400
+
+// Save your bankAccountSummary total to a variable called bankTotal
+const bankTotal = bankAccountSummary(bankInfo);
+
+// Write another function called inTheRed that accepts your bank account summary total (number) as an argument. The function should return true if your total is less than 0, and false otherwise.
+
+  function inTheRed(total) {
+    if (total < 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+// console.log(inTheRed(bankTotal)); // Output: true
+// assume my bank total is -400
+inTheRed(bankTotal) // returns true
